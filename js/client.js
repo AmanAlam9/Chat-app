49ae7067d9a61e52c58b9984c4e978451a909e15
Aman Alam <amanalam896980@gmail.com>
2022-01-19 22:26:18 +0530

    Initial commit

---
+++ js/client.js
@@ -0,0 +1,50 @@
+const socket=io('http://localhost:8000');
+
+//const form=document.getElementById('send-container');
+ // const form=document.querySelector('#send-container');
+ const form=document.querySelector(".send");
+ const messageInput=document.getElementById('messageInp');
+ const messageContainer=document.querySelector(".container");
+
+ var audio=new Audio('chattune.mp3');
+
+
+ const append=(message,position)=>{
+      const messageElement=document.createElement('div');
+      messageElement.innerText=message;
+      messageElement.classList.add('message');
+      messageElement.classList.add(position);
+      messageContainer.append(messageElement);
+      if(position =='left'){
+        audio.play();
+      }
+     
+ }
+
+form.addEventListener('submit',(e)=>{
+      e.preventDefault();
+      const message=messageInput.value;
+      append(`You: ${message}`,'right');
+      socket.emit('send',message);
+      messageInput.value='';
+
+ });
+ 
+
+ const name=prompt("Enter your name");
+ socket.emit('new-user-joined',name);
+
+ socket.on('user-joined',name=>{
+     append(`${name} Joined chat`,'right'); 
+ })
+
+ socket.on('recieve',data=>{
+    append(`${data.name}: ${data.message}`,'left');
+})
+
+socket.on('left',name=>{
+    append(`${name} left `,'right');
+})
+
+ 
+
